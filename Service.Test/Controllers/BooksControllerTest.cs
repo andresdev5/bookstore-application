aa90c2db9ca74254fef2044ae61410d98820fdbb
Andrés Jácome <ajjacome2@espe.edu.ec>
2024-06-24 14:25:56 -0500

    diagrams

--- Service.Test/Controllers/BooksControllerTest.cs
+++ Service.Test/Controllers/BooksControllerTest.cs
@@ -74,6 +74,48 @@ namespace Service.Test.Controllers
 
 			var response = await controller.AddBook(bookRequestModel);
 			var result = Assert.IsType<OkResult>(response);
+			mockService.Verify(x => x.AddBook(It.IsAny<BookRequestModel>()));
+		}
+
+		[Fact]
+		public async Task ShouldUpdateBook()
+		{
+			var bookRequestModel = new BookRequestModel
+			{
+				Id = 1,
+				Title = "Updated Book",
+				Year = 2022,
+				Publisher = "Publisher",
+				ISBN = "ISBN",
+				Price = 19.99m,
+				Stock = 10
+			};
+			var mockService = new Mock<IBookService>();
+			mockService.Setup(svc => svc.UpdateBook(bookRequestModel)).Returns(Task.FromResult(true));
+			var controller = new BooksController(mockService.Object);
+
+			var response = await controller.UpdateBook(bookRequestModel);
+			var result = Assert.IsType<OkResult>(response);
+		}
+
+		[Fact]
+		public async Task ShouldDeleteBook()
+		{
+			int id = 1;
+			Book book = new()
+			{
+				Id = id,
+				Title = "Test Book"
+			};
+			var mockService = new Mock<IBookService>();
+			mockService.Setup(svc => svc.GetBookById(id)).Returns(Task.FromResult<Book>(book));
+			mockService.Setup(svc => svc.DeleteBook(book)).Returns(Task.FromResult(true));
+			var controller = new BooksController(mockService.Object);
+
+			var response = await controller.DeleteBook(id);
+			var result = Assert.IsType<OkResult>(response);
+			mockService.Verify(x => x.GetBookById(It.IsAny<int>()));
+			mockService.Verify(x => x.DeleteBook(It.IsAny<Book>()));
 		}
 	}
 }
